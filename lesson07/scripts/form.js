f0e950eadd927613b95f745f22698a0497c0f641
shanebasham <shanebasham28@gmail.com>
2024-03-02 20:51:19 -0700

    form assignment

---
+++ lesson07/scripts/form.js
@@ -0,0 +1,55 @@
+const kp1 = document.querySelector("#keyphrase");
+const kp2 = document.querySelector("#keyphrase2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	// Function to handle form submission
+	function handleSubmit(event) {
+		// Prevent default form submission
+		event.preventDefault();
+
+		// Access form elements
+		let formt = event.target;
+		let formData = new FormData(formt);
+
+		// Display form element values
+		for (let pair of formData.entries()) {
+			console.log(pair[0] + ": " + pair[1]);
+		}
+	}
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", handleSubmit);
+});
+
+
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+};
