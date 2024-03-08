16b6a8e0dfa5b93396cfcfd8ee53928df0c823f0
shanebasham <shanebasham28@gmail.com>
2024-03-07 19:13:25 -0700

    lesson09

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,32 @@
+
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+  
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement("section");
+        const fullName = document.createElement("h2");
+        const portrait = document.createElement("img");
+
+        fullName.textContent = `${prophet.name.first} ${prophet.name.last}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name.first} ${prophet.name.last}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+};
