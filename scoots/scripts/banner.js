e9cd7e18dbf5218a868a856122782e7b2dc61b1d
shanebasham <shanebasham28@gmail.com>
2024-04-01 13:50:56 -0600

    scoots fixed edits

--- scoots/scripts/banner.js
+++ scoots/scripts/banner.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Close banner
 const banner = document.getElementById('banner');
 const bannerCloseButton = document.getElementById('banner-close-button');
@@ -12,20 +11,4 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=island&appid=e3c029f95
 .then(response => response.json())
 .then(data => {
   document.getElementById('high-temp').innerText = data.list[0].main.temp_max;
-=======
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const today = new Date();
-const dayOfWeek = daysOfWeek[today.getDay()];
-const banner = document.getElementById('chamber-banner');
-
-// Only display the banner on Monday, Tuesday, or Wednesday
-if (dayOfWeek === 'Monday' || dayOfWeek === 'Tuesday' || dayOfWeek === 'Wednesday') {
-    banner.style.display = 'block';
-}
-
-// Add a click event listener to the close button to hide the banner
-const bannerCloseButton = document.getElementById('banner-close-button');
-bannerCloseButton.addEventListener('click', () => {
-    banner.style.display = 'none';
->>>>>>> 3602172e894db010dbd557f10b898f854c44d006
 });
